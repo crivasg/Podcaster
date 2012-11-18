2d49b01a75dc19468d3d04da22906bea2d9c1769
Cesar A. Rivas <crivasg@gmail.com>
2012-11-17 21:50:11 -0500

    Added utilities to the projecct

---
+++ Podcaster/Utilities.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Podcaster
+{
+    public static class Utilities
+    {
+        public static String HumanReadableFileSize(double size)
+        {
+            string[] units = { "B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB" };
+            int unit = 0;
+            while (size >= 1024)
+            {
+                size /= 1024;
+                ++unit;
+            }
+
+            return String.Format("{0:G4} {1}", size, units[unit]);
+        }
+
+    }
+}
