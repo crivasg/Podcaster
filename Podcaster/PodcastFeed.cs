d8cf6107334001da22dfb8af3dfab5d14695a65b
Cesar A. Rivas <crivasg@gmail.com>
2012-11-25 17:47:40 -0500

    Added PodcastFeed class

---
+++ Podcaster/PodcastFeed.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Podcaster
+{
+    class PodcastFeed
+    {
+        public String URL { private get; set; }
+        public String[] Episodes { get; private set; }
+
+        public PodcastFeed()
+        { 
+        }
+
+    }
+}
