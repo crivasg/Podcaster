35e901e6a694e7fb10b3510e8bcbd31941722f99
Cesar A. Rivas <crivasg@gmail.com>
2012-12-02 15:52:15 -0500

    Get the information of the RSS using the code proposed by oppositional , http://stackoverflow.com/users/2029/oppositional

--- Podcaster/PodcastFeed.cs
+++ Podcaster/PodcastFeed.cs
@@ -9,6 +9,7 @@ using System.ServiceModel;
 using System.ServiceModel.Syndication;
 using System.Xml;
 using System.Xml.Linq;
+using System.Xml.XPath;
 
 namespace Podcaster
 {
@@ -32,7 +33,7 @@ namespace Podcaster
             {
                 SyndicationFeed feedContent = SyndicationFeed.Load(feedReader);
                 //XmlQualifiedName n = new XmlQualifiedName("itunes", "http://www.w3.org/2000/xmlns/");
-                //String itunesNs = "http://www.itunes.com/dtds/podcast-1.0.dtd";
+                String itunesNs = "http://www.itunes.com/dtds/podcast-1.0.dtd";
                 //feedContent.AttributeExtensions.Add(n, itunesNs);
                 if (feedContent == null)
                 {
@@ -42,13 +43,48 @@ namespace Podcaster
                 foreach (SyndicationItem item in feedContent.Items)
                 {
                     SyndicationPerson author = item.Authors[0];
-                    
-                    titleList.Add(item.Title.Text + "\n" + author.Email + "\n" + item.PublishDate.LocalDateTime.ToString() + "\n" + item.Summary.Text);
 
-                    //foreach (SyndicationLink links in item.Links.Where(links => links.RelationshipType == "enclosure"))
-                    foreach (SyndicationLink links in item.Links)
+                    // Get values of syndication extension elements for a given namespace
+                    string extensionNamespaceUri = "http://www.itunes.com/dtds/podcast-1.0.dtd";
+                    SyndicationElementExtension extension = item.ElementExtensions.Where<SyndicationElementExtension>(x => x.OuterNamespace == extensionNamespaceUri).FirstOrDefault();
+                    XPathNavigator dataNavigator = new XPathDocument(extension.GetReader()).CreateNavigator();
+
+                    XmlNamespaceManager resolver = new XmlNamespaceManager(dataNavigator.NameTable);
+                    resolver.AddNamespace("itunes", extensionNamespaceUri);
+
+                    XPathNavigator authorNavigator = dataNavigator.SelectSingleNode("itunes:author", resolver);
+                    XPathNavigator subtitleNavigator = dataNavigator.SelectSingleNode("itunes:subtitle", resolver);
+                    XPathNavigator summaryNavigator = dataNavigator.SelectSingleNode("itunes:summary", resolver);
+                    XPathNavigator durationNavigator = dataNavigator.SelectSingleNode("itunes:duration", resolver);
+                    XPathNavigator imageNavigator = dataNavigator.SelectSingleNode("itunes:image", resolver);
+                    String imageOuterXML = imageNavigator.OuterXml;
+
+                    String imageUrl = String.Empty;
+
+                    if (imageNavigator.MoveToFirstAttribute())
                     {
+                        imageUrl = imageNavigator.Value.ToString();
+                        // go back from the attributes to the parent element
+                        imageNavigator.MoveToParent();
+                    }
+                 
 
+                    string authorx = authorNavigator != null ? authorNavigator.Value : String.Empty;
+                    string subtitle = subtitleNavigator != null ? subtitleNavigator.Value : String.Empty;
+                    string summary = summaryNavigator != null ? summaryNavigator.Value : String.Empty;
+                    string duration = durationNavigator != null ? durationNavigator.Value : String.Empty;
+                    
+                    foreach (SyndicationLink links in item.Links.Where<SyndicationLink>(links => links.RelationshipType == "enclosure"))
+                    {
+                        Uri url = links.Uri;
+                        long length = links.Length;
+                        string mediaType = links.MediaType;
+
+                        titleList.Add(item.Title.Text + "\n" + author.Email + "\n" + 
+                            item.PublishDate.LocalDateTime.ToString() + "\n" +
+                            url.ToString() + " " + length.ToString() + " " + mediaType +
+                            "\n" + imageUrl +
+                            "\n\n" + item.Summary.Text);
                         
                     }
 
